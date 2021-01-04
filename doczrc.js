2db7937eb93cce9f4aa268804317abd2a085d62e
Jakub Biesiada <kbba@op.pl>
2021-01-04 20:22:55 +0100

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,11 @@ export default {
   title: 'React WordPress Hooks',
   themeConfig: {
     colors: {
-      primary: '#0073aa',
+      header: {
+        button: {
+          bg: '#0073aa',
+        },
+      },
     },
   },
   files: 'docs/*.md',
